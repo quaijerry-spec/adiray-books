2366dce6825d87020c3804c111a9e7ea81308d7a
quaijerry-spec <quaijerry@gmail.com>
2026-01-28 23:19:21 +0200

    Create Cart.jsx

---
+++ src/components/Cart.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Cart() {
+  return (
+    <section>
+      <h2>Your Cart</h2>
+      <p>Cart items will appear here.</p>
+    </section>
+  );
+}
