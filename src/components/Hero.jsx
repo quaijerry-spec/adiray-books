e808bacf0b1b52342f5365077d024e4b5139f976
quaijerry-spec <quaijerry@gmail.com>
2026-01-28 23:17:37 +0200

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,1 +1,10 @@
+import React from "react";
+import logo from "../../public/logo-background.png"; // optional
 
+export default function Hero() {
+  return (
+    <section style={{ backgroundImage: `url('/logo-background.png')` }}>
+      <h1>Welcome to AdiRay Books</h1>
+    </section>
+  );
+}
