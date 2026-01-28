8a243660b2168815967d9db55ab2ac8289e68d84
quaijerry-spec <quaijerry@gmail.com>
2026-01-28 23:18:20 +0200

    Update FeaturedBooks.jsx

--- src/components/FeaturedBooks.jsx
+++ src/components/FeaturedBooks.jsx
@@ -1,1 +1,24 @@
+import React from "react";
 
+const books = [
+  { title: "The Way of the Superior Man", author: "David Deida", price: 15, genre: "Self-help" },
+  { title: "The 50th Law", author: "50 Cent & Robert Greene", price: 13, genre: "Biography" }
+];
+
+export default function FeaturedBooks() {
+  return (
+    <section>
+      <h2>Featured Books</h2>
+      <div>
+        {books.map((book, idx) => (
+          <div key={idx}>
+            <h3>{book.title}</h3>
+            <p>Author: {book.author}</p>
+            <p>Price: ${book.price}</p>
+            <p>Genre: {book.genre}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
