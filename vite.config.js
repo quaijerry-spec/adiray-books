0425dd423a7cc093db6124538906ca92663f2593
quaijerry-spec <quaijerry@gmail.com>
2026-02-01 13:57:35 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: {
-    outDir: 'dist'
-  }
-})
+  plugins: [react()],
+});
