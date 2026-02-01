4880ed1ff21f2bf0e9a6ca2cd1c88669bcd90b73
quaijerry-spec <quaijerry@gmail.com>
2026-02-01 14:49:26 +0200

    Update books.js

--- src/books.js
+++ src/books.js
@@ -4,11 +4,15 @@ const books = [
     title: "The Way of the Superior Man",
     author: "David Deida",
     price: 15,
+    "/books/way-of-superior-man.jpg"
+  },
   {
     id: 2,
     title: "The 50th Law",
     author: "50 Cent & Robert Greene",
     price: 13,
+    "/books/50th-law.jpg"
+  }
 ];
 
 export default books;
