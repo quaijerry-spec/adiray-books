9dc92e8695484b53c941532480e00add40746bf5
quaijerry-spec <quaijerry@gmail.com>
2026-02-01 14:37:50 +0200

    Create books.js

---
+++ src/books.js
@@ -0,0 +1,18 @@
+const books = [
+  {
+    id: 1,
+    title: "The Way of the Superior Man",
+    author: "David Deida",
+    price: 15,
+    image: "/books/way-of-superior-man.jpg"
+  },
+  {
+    id: 2,
+    title: "The 50th Law",
+    author: "50 Cent & Robert Greene",
+    price: 13,
+    image: "/books/50th-law.jpg"
+  }
+];
+
+export default books;
