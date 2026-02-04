c5b475f0fea6a9d075975951da7fc47e6ff8c7ce
quaijerry-spec <quaijerry@gmail.com>
2026-02-04 07:49:07 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import { useCart } from "../context/CartContext";
-import logo from "../assets/logo.png";
 
 export default function Navbar({ toggleCart }) {
   const { cart } = useCart();
@@ -9,10 +8,10 @@ export default function Navbar({ toggleCart }) {
       <nav className="flex items-center justify-between w-[95%] max-w-6xl bg-gray-600 text-white px-6 py-3 rounded-full shadow-lg">
         <div className="flex items-center gap-3">
           <img
-  src="/logo.png"
-  alt="AdiRay Books"
-  className="h-10 w-10"
-/>
+            src="/logo.png"
+            alt="AdiRay Books"
+            className="h-10 w-10"
+          />
           <span className="font-bold text-lg text-yellow-400">
             AdiRay Books
           </span>
