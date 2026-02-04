2a83a0a339b60ce6fd8a4f06fdf97e8230627cfc
quaijerry-spec <quaijerry@gmail.com>
2026-02-04 10:32:47 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import CartProvider from "./context/CartContext";
+import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
-  </React.StrictMode>
+  <CartProvider>
+    <App />
+  </CartProvider>
 );
