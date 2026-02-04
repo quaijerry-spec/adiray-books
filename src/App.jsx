f4b63cdfb8ab8b0625c82d48cee4261f2d1cbade
quaijerry-spec <quaijerry@gmail.com>
2026-02-04 10:07:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import { useState } from "react";
-import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 import Books from "./components/Books";
 import Cart from "./components/Cart";
+import CartProvider from "./context/CartContext";
 
 export default function App() {
   const [showCart, setShowCart] = useState(false);
-  const [search, setSearch] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <div className="min-h-screen bg-[#0f172a]">
-      <Navbar
-        onToggleCart={() => setShowCart(!showCart)}
-        onSearch={setSearch}
-      />
-
-      {!showCart && <Hero />}
-
-      {showCart && <Cart onClose={() => setShowCart(false)} />}
-
-      {!showCart && <Books search={search} />}
-    </div>
+    <CartProvider>
+      <Navbar onSearch={setSearchQuery} />
+      {showCart && <Cart />}
+      <Hero />
+      <Books searchQuery={searchQuery} />
+    </CartProvider>
   );
 }
