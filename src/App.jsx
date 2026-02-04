d999d939e4ed27e6dbe413a4547609a7eea38076
quaijerry-spec <quaijerry@gmail.com>
2026-02-04 09:14:21 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,10 @@ export default function App() {
   const [showCart, setShowCart] = useState(false);
 
   return (
-    <div className="bg-white min-h-screen">
+    <>
       <Navbar toggleCart={() => setShowCart(!showCart)} />
-
-      {!showCart && <Hero />}
-
-      <main className="max-w-6xl mx-auto px-6 mt-16">
-        {showCart ? <Cart /> : <Books />}
-      </main>
-    </div>
+      <Hero />
+      {showCart ? <Cart /> : <Books />}
+    </>
   );
 }
