9f68cd1330f58f08b155adfb8313ec561ef74fb3
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 00:11:17 +0200

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Hero from "../components/Hero";
+import Books from "../components/Books";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Books />
+    </>
+  );
+}
