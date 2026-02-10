7b168c2fdc1ee8950ffb56cc26154dc04ea27a69
quaijerry-spec <quaijerry@gmail.com>
2026-02-10 15:57:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 import Books from "./components/Books";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import Admin from "./components/Admin";
 
-function App() {
+export default function App() {
+  const [cart, setCart] = useState([]);
+
+  function addToCart(book) {
+    setCart([...cart, book]);
+  }
+
+  function removeFromCart(index) {
+    setCart(cart.filter((_, i) => i !== index));
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Books />} />
-      </Routes>
-    </Router>
+    <>
+      <h1 style={{ textAlign: "center" }}>AdiRay Books</h1>
+
+      <Books addToCart={addToCart} />
+      <Cart cart={cart} removeFromCart={removeFromCart} />
+      <Checkout cart={cart} />
+      <Admin />
+    </>
   );
 }
-
-export default App;
