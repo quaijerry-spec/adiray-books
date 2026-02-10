e0f13c24e5f78e685a79d400232ab3cfff2e188a
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 00:16:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import { motion } from "framer-motion";
-
-import Home from "./pages/Home";
-import Cart from "./pages/Cart";
 import Hero from "./components/Hero";
 import Books from "./components/Books";
 import Checkout from "./pages/Checkout";
 
 export default function App() {
   return (
-    <>
-      {/* HERO SECTION */}
-      <Hero />
-
-      {/* PAGE CONTENT */}
-      <motion.main
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.6 }}
-      >
-        <Routes>
-          <Route path="/" element={<Books />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-      </motion.main>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <>
+            <Hero />
+            <Books />
+          </>
+        }
+      />
+      <Route path="/checkout" element={<Checkout />} />
+    </Routes>
   );
 }
