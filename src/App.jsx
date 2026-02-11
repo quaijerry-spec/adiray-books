364b6945bb20d02d0cfa0f7f0e816a5491facb1e
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 23:23:39 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import Cart from "./pages/Cart";
 import { CartProvider } from "./context/CartContext";
 import Home from "./pages/Home";
 
@@ -6,6 +7,7 @@ export default function App() {
   return (
     <CartProvider>
       <Routes>
+        <Route path="/cart" element={<Cart />} />
         <Route path="/" element={<Home />} />
       </Routes>
     </CartProvider>
