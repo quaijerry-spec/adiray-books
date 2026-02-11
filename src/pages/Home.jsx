3ac0e5cbe980c7c33e9c922278382740528075ee
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 23:45:54 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,31 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <>
+      {/* Header with Cart button */}
+      <header style={{
+        display: "flex",
+        justifyContent: "flex-end",
+        padding: "20px",
+        backgroundColor: "#2c5364",
+      }}>
+        <Link 
+          to="/cart" 
+          style={{
+            color: "white",
+            textDecoration: "none",
+            fontWeight: "bold",
+            fontSize: "16px",
+            backgroundColor: "#203a43",
+            padding: "10px 15px",
+            borderRadius: "8px",
+          }}
+        >
+          🛒 Cart
+        </Link>
+      </header>
+
+      {/* Main content */}
       <Hero />
-
-      {/* Cart Link */}
-      <div style={{ textAlign: "center", margin: "1rem 0" }}>
-        <Link to="/cart">Go to Cart 🛒</Link>
-      </div>
-
       <Books />
     </>
   );
