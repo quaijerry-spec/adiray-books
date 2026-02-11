ba08650aaa5c7ddc7ac2d9640d8e6c50cf1fd705
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 21:09:44 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
-import { CartProvider } from "./context/CartContext"; // adjust path if needed
+// src/App.jsx
+import React from "react";
+import { CartProvider } from "./context/CartContext"; // make sure the path is correct
 import Home from "./pages/Home";
+
 function App() {
   return (
     <CartProvider>
