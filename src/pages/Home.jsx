ae2de2b06d2b0dfb8b1fef1192515e34d5a5a5e4
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 23:35:32 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import Hero from "../components/Hero";
 import Books from "../components/Books";
 import { Link } from "react-router-dom";
-<Link to="/cart">Go to Cart 🛒</Link>
 
 export default function Home() {
   return (
     <>
       <Hero />
+
+      {/* Cart Link */}
+      <div style={{ textAlign: "center", margin: "1rem 0" }}>
+        <Link to="/cart">Go to Cart 🛒</Link>
+      </div>
+
       <Books />
     </>
   );
