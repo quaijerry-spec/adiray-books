66caeb7c4c196d4ea71abbe7079f1e64cdfbf0f4
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 23:57:02 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,46 +5,13 @@ import { useCart } from "../context/CartContext";
 
 export default function Home() {
   const { cart } = useCart();
-
   const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
   return (
     <>
-      {/* Cart header */}
-      <header style={{
-        display: "flex",
-        justifyContent: "flex-end",
-        padding: "20px",
-        backgroundColor: "#2c5364",
-      }}>
-        <Link
-          to="/cart"
-          style={{
-            color: "white",
-            textDecoration: "none",
-            fontWeight: "bold",
-            fontSize: "16px",
-            backgroundColor: "#203a43",
-            padding: "10px 15px",
-            borderRadius: "8px",
-            display: "flex",
-            alignItems: "center",
-          }}
-        >
-          🛒 Cart
-          {totalItems > 0 && (
-            <span style={{
-              marginLeft: "8px",
-              backgroundColor: "red",
-              color: "white",
-              borderRadius: "50%",
-              padding: "2px 8px",
-              fontSize: "14px",
-              fontWeight: "bold",
-            }}>
-              {totalItems}
-            </span>
-          )}
+      <header style={{ display: "flex", justifyContent: "flex-end", padding: "20px" }}>
+        <Link to="/cart" style={{ textDecoration: "none" }}>
+          🛒 Cart {totalItems > 0 && `(${totalItems})`}
         </Link>
       </header>
 
