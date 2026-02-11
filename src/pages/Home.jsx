0de212eb19d9e0579e5bdf78d0c6471a812798b9
quaijerry-spec <quaijerry@gmail.com>
2026-02-11 23:27:57 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Hero from "../components/Hero";
 import Books from "../components/Books";
+import { Link } from "react-router-dom";
+<Link to="/cart">Go to Cart 🛒</Link>
 
 export default function Home() {
   return (
