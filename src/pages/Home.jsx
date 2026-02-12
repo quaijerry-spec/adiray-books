4e81e05f644b4b239bdbd643fef529ca0d56d123
quaijerry-spec <quaijerry@gmail.com>
2026-02-13 00:40:51 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
-export default function Home() {
-  const [search, setSearch] = useState("");
-
+export default function Home({ search, setSearch }) {
   const books = [
     {
       id: 1,
