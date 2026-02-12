82039189007ee86ef65e5dd3bb05f2460676bb80
quaijerry-spec <quaijerry@gmail.com>
2026-02-12 18:51:08 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "20px", background: "#f1f1f1" }}>
+      © 2026 AdiRay Books
+    </footer>
+  );
+}
