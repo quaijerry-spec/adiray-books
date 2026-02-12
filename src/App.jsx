a792b11fd26061aa16576ceebf3dde974b372247
quaijerry-spec <quaijerry@gmail.com>
2026-02-13 00:41:58 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Routes, Route } from "react-router-dom";
@@ -6,16 +7,21 @@ import Cart from "./pages/Cart";
 import { CartProvider } from "./context/CartContext";
 
 export default function App() {
+  const [search, setSearch] = useState("");
+
   return (
     <CartProvider>
-      <div className="bg-gray-100 min-h-screen">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-        </Routes>
-        <Footer />
-      </div>
+      <Navbar search={search} setSearch={setSearch} />
+
+      <Routes>
+        <Route
+          path="/"
+          element={<Home search={search} setSearch={setSearch} />}
+        />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+
+      <Footer />
     </CartProvider>
   );
 }
