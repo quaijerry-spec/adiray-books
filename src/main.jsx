e8b0923fe6cae7a8653258384ed95ea6f90cd3cc
quaijerry-spec <quaijerry@gmail.com>
2026-02-13 20:00:02 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,10 +7,10 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-   <BrowserRouter>
-  <CartProvider>
-    <App />
-  </CartProvider>
-</BrowserRouter> 
+    <BrowserRouter>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
