cd7ad4d823c322c9e4ba853c653a709375727e80
quaijerry-spec <quaijerry@gmail.com>
2026-02-14 02:08:12 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+// src/App.jsx
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
+import { CartProvider } from "./context/CartContext";
 
 export default function App() {
   return (
-    <>
+    <CartProvider>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -15,6 +17,6 @@ export default function App() {
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
       <Footer />
-    </>
+    </CartProvider>
   );
 }
