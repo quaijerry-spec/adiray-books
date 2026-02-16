9e2ebb8f00be8e1e0cd29da7c318b6ca809724f0
quaijerry-spec <quaijerry@gmail.com>
2026-02-16 13:08:57 +0200

    Update Account.jsx

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import React, { useState, useEffect } from "react";
 import { auth } from "../firebase";
 import { GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "firebase/auth";
