3ad2a65399b0f8dcc4d38a34fb6b51044bc7725c
quaijerry-spec <quaijerry@gmail.com>
2026-02-17 12:02:52 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,20 +2,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyBd_q9GpUeLERYaAhTfSksx3eKDPchczFQ",
   authDomain: "adiray-books-1234.firebaseapp.com",
   projectId: "adiray-books-1234",
   storageBucket: "adiray-books-1234.appspot.com",
   messagingSenderId: "554330985579",
-  appId: "1:554330985579:web:14778bd9a56ea0f159cd2e"
+  appId: "1:554330985579:web:14778bd9a56ea0f159cd2e",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export Auth to use in your app
+// Export auth so other files can use it
 export const auth = getAuth(app);
-
-export default app;
